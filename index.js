daf94197df999472304af8011487049901055258
florahaus <floraahaus@gmail.com>
2023-02-08 16:54:29 -0300

    Updates

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 import validator from "./validator.js";
-function validacao (e){
+
+function validacao(e) {
   e.preventDefault();
 
-  const numCard = document.getElementById ("cartao")
-  
-  const aviso = document.getElementById ("mensagem")
+  const numCard = document.getElementById("cartao");
+  const aviso = document.getElementById("mensagem");
 
-  const  digito = numCard.value;
+  const digito = numCard.value;
+  
+  if (digito.trim() === "" || digito.length <= 4) {
+    aviso.innerHTML = "Insira o número do cartão para validar";
+    return;
+  }
+
   const validar = validator.isValid(digito);
   const mascara = validator.maskify(digito);
-  numCard.value = mascara
+  numCard.value = mascara;
   
-  if (
-    digito.trim() === "" ||    
-      digito === "0000000000000000" ||
-      digito === "000000000000000" ||
-      digito === "00000000000000" ||
-      digito.length <= 13
-  ) {
-    return (aviso.innerHTML = "Insira o número para validar");
+  if (validar) {
+    aviso.innerHTML = `Seu cartão está válido! ${validator.maskify(digito)}`;
+  } else {  
+    aviso.innerHTML = `Cartão inválido! Tente novamente. ${validator.maskify(digito)}`;
   }  
-  else if (validar === true) {
-    return (aviso.innerHTML = `"Seu cartão está válido!" ${validator.maskify(digito)}`);
-  }
-  else if (!validar) {  
-    return (aviso.innerHTML = `"Cartão inválido! Tente novamente." ${validator.maskify(digito)}`);
-  }   
 }
-const button = document.getElementById   ("botao")
-button.addEventListener("click",validacao);
+
+const button = document.getElementById("botao");
+button.addEventListener("click", validacao);
 
   
 
